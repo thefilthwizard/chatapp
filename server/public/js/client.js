91c0a9b90bb9c637bca4f18d4f065c6d9639287c
Johan Viljoen <ChristoffelViljoen@discovery.bank>
2019-01-10 09:16:01 +0200

    weeee

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,4 +1,4 @@
-getMessages()
+getMessages();
 
 $(() => {
   $('#send').click(() => {
@@ -6,40 +6,40 @@ $(() => {
       id: 999,
       name: $('#name').val(),
       message: $('#message').val()
-    })
-    $('#message').empty()
-  })
-})
+    });
+    $('#message').empty();
+  });
+});
 
-const socket = io()
+const socket = io();
 
 socket.on('message', () => {
-  console.log('get emmit from socket.io')
-  latestMsg()
+  console.log('get emmit from socket.io');
+  latestMsg();
 })
 
 function addMessages(message) {
   $('#messages').append(`
     <h4> ${message.name} </h4>
-    <p>  ${message.message} </p>`)
+    <p>  ${message.message} </p>`);
 }
 
 function getMessages() {
-  $('#messages').empty()
+  $('#messages').empty();
   $.get('http://192.243.100.152:8099/call', (data) => {
-    data.forEach(addMessages)
-  })
-  window.scrollTo(0, document.body.scrollHeight)
+    data.forEach(addMessages);
+  });
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function latestMsg() {
   $.get('http://192.243.100.152:8099/call', (data) => {
-    const lastMsg = data[data.length - 1]
-    addMessages(lastMsg)
+    const lastMsg = data[data.length - 1];
+    addMessages(lastMsg);
   })
-  window.scrollTo(0, document.body.scrollHeight)
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function sendMessage(message) {
-    $.post('http://192.243.100.152:8099/msg', message)
+    $.post('http://192.243.100.152:8099/msg', message);
 }
--- server/server.js
+++ server/server.js
@@ -1,83 +1,83 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 // const spawn = require('child_process').spawn;
 // const fs = require('fs');
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const path = require('path')
-// const ltunnel = require('localtunnel')
-require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+// const ltunnel = require('localtunnel');
+require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
-}))
+}));
 
 const Message = mongoose.model('Message', {
   id: Number,
   name: String,
   message: String
-})
+});
 
-const dbUrl = 'mongodb://localhost:27017/dirtychat' // setup mongo db
+const dbUrl = 'mongodb://localhost:27017/dirtychat'; // setup mongo db
 
 mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => {
-  console.log('mongodb connected', err)
-})
+  console.log('mongodb connected', err);
+});
 
 io.on('connection', () => {
-  console.log('a user is connected')
-})
+  console.log('a user is connected');
+});
 
 app.post('/msg', (req, res) => {
   try {
-    const message = new Message(req.body)
+    const message = new Message(req.body);
     const savedMessage = message.save((err) => {
-      res.sendStatus(200)
-      console.log(err)
-    })
-    console.log(savedMessage)
+      res.sendStatus(200);
+      console.log(err);
+    });
+    console.log(savedMessage);
   } catch (error) {
-    res.sendStatus(500)
-    return console.log('post error:' + error)
+    res.sendStatus(500);
+    return console.log('post error:' + error);
   } finally {
-    console.log('Message Posted')
-    io.emit('message')
+    console.log('Message Posted');
+    io.emit('message');
   }
-})
+});
 
 app.get('/messages/:user', (req, res) => {
-  const user = req.params.user
+  const user = req.params.user;
   Message.find({ name: user }, (err, messages) => {
-    res.send(messages)
-    console.log(err)
-  })
-})
+    res.send(messages);
+    console.log(err);
+  });
+});
 
 app.get('/call', (req, res) => {
   Message.find({}, (err, messages) => {
-    res.send(messages)
-    console.log(err)
-  })
-})
+    res.send(messages);
+    console.log(err);
+  });
+});
 
 app.get('/ping', (req, res) => {
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 app.get('/', (req, res) => {
-  const pathToIndex = path.join(__dirname, '/public/', 'index.html')
-  res.sendFile(pathToIndex)
-})
+  const pathToIndex = path.join(__dirname, '/public/', 'index.html');
+  res.sendFile(pathToIndex);
+});
 
 const server = http.listen(8099, () => {
-  const host = server.address().address
-  const port = server.address().port
-  console.log('listening on host: ' + host + ' port no: ' + port)
-})
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('listening on host: ' + host + ' port no: ' + port);
+});
 
 /* const tunnel = ltunnel(8099, { subdomain: 'filthwizard' }, (err, tun) => {
    if (err) console.log('tunnel error: ' + err);
