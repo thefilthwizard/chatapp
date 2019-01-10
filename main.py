a3e4853d3a9938ff892e5b641e8095480342c0ca
Johan Viljoen <ChristoffelViljoen@discovery.bank>
2019-01-10 08:59:26 +0200

    some curese testing

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from unicurses import *
 
 
 def main():
-    print "hello"
+    stdscr = initscr()
+    start_color()
+    init_pair(1, COLOR_BLUE, COLOR_BLACK)
+    init_pair(2, COLOR_RED, COLOR_BLUE)
+    window = newwin(5,5,5,5)
+    waddstr(window, "test", color_pair(2) + A_BOLD)
+    wgetch(window)
+    addstr("Hello Curses\n", color_pair(1) + A_BOLD)
+    addstr("press <anykey> to exit")
+    getch()
+    endwin()
     return 0
 
 
