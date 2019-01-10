af5f255a62e9848c0ecdbba2c625ec9051bc67a6
Johan Viljoen <ChristoffelViljoen@discovery.bank>
2019-01-10 13:34:03 +0200

    python post msg

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 
 GETURL = 'http://192.243.100.152:8099/call'
 PINGURL = 'http://192.243.100.152:8099/ping'
+POSTURL = 'http://192.243.100.152:8099/msg'
 
 socket = socketio.Client()
 
@@ -13,14 +14,14 @@ def getLastMsg():
     return 0
 
 @socket.on('userconnected')
-def showUserConnected():
-    time.sleep(0.5)
+def showUserConnected():    
     addstr('\nuser connected\n', color_pair(3) + A_BOLD)
 
 def main():
     socket.connect('http://192.243.100.152:8099')
     stdscr = initscr()
     start_color()
+    time.sleep(5)
     ping = requests.get(url = PINGURL)
     init_pair(1, COLOR_BLUE, COLOR_BLACK)
     init_pair(2, COLOR_RED, COLOR_BLACK)
@@ -29,6 +30,13 @@ def main():
         addstr('Connected to server\n', color_pair(1) + A_BOLD)
     else:
         addstr('Server Connection failed\n', color_pair(2) + A_BOLD)
+    # ========================================================
+    # this will need to go to a fucntion call to send messages
+    # --------------------------------------------------------
+    msgsent = requests.post(POSTURL, { 'id': 999, 'name': 'python', 'message': '#test message from python/curses' })
+    if msgsent.status_code == 200:
+        addstr('\nMessage posted on server\n', color_pair(2))   
+    # ========================================================
     addstr('\npress <anykey> to exit')
     getch()
     endwin()
