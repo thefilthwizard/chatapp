ba78abf5f49f3179a4d953a7a8bd216efe1a0a1c
Johan Viljoen <ChristoffelViljoen@discovery.bank>
2019-01-10 09:44:48 +0200

    i dunno

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,5 +1,7 @@
 getMessages();
 
+const msgDiv = document.getElementById('messages');
+
 $(() => {
   $('#send').click(() => {
     sendMessage({
@@ -16,7 +18,7 @@ const socket = io();
 socket.on('message', () => {
   console.log('get emmit from socket.io');
   latestMsg();
-})
+});
 
 function addMessages(message) {
   $('#messages').append(`
@@ -29,15 +31,15 @@ function getMessages() {
   $.get('http://192.243.100.152:8099/call', (data) => {
     data.forEach(addMessages);
   });
-  $('#messages').scrollTop = $('#messages').scrollHeight;
+  msgDiv.scrollTop = msgDiv.scrollHeight;
 }
 
 function latestMsg() {
   $.get('http://192.243.100.152:8099/call', (data) => {
     const lastMsg = data[data.length - 1];
     addMessages(lastMsg);
-  })
-  $('#messages').scrollTop = $('#messages').scrollHeight;
+  });
+  msgDiv.scrollTop = msgDiv.scrollHeight;
 }
 
 function sendMessage(message) {
